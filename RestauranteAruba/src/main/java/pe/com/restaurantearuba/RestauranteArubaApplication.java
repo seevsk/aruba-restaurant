3800ff695fbcd9d80d28290f7bd23d478731bce4
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-07-19 11:57:29 -0500

    first commit

---
+++ RestauranteAruba/src/main/java/pe/com/restaurantearuba/RestauranteArubaApplication.java
@@ -0,0 +1,13 @@
+package pe.com.restaurantearuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteArubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteArubaApplication.class, args);
+	}
+
+}
---
+++ RestauranteAruba/src/test/java/pe/com/restaurantearuba/RestauranteArubaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.restaurantearuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteArubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
